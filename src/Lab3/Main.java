e13beb4a75c3dfbe423893c7aa89bbb52be790ae
NguyenDinhHuong30088 <70881463+NguyenDinhHuong30088@users.noreply.github.com>
2021-03-08 09:27:32 +0700

    upcode

--- src/Lab3/Main.java
+++ src/Lab3/Main.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Room r = new Room();
-        r.setName("Phong so 1");
-        r.setPosition("Vinh Phuc");
-        r.addUser("Truong Quang Huy");
-        r.addUser("Dao Minh Giang");
-        r.addUser("Hoàng Văn Sơn");
+        r.setName("Phong 1");
+        r.setPosition("Ha Noi");
+        r.addUser("Huong");
+        r.addUser("Thinh");
+        r.addUser("Dat");
         r.removeUser(2);
         r.printRoom();
     }
