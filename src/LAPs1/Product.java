e443868bad82fbc832cf7252e2367dcc97aebdc1
NguyenDinhHuong30088 <70881463+NguyenDinhHuong30088@users.noreply.github.com>
2021-03-08 09:21:56 +0700

    upcode

---
+++ src/LAPs1/Product.java
@@ -0,0 +1,33 @@
+package LAPs1;
+
+public class Product {
+    int Product_code;
+    String Product_name;
+    float Price;
+    int Amount;
+    String Unit;
+
+    //Hiển thị
+    void run(){
+        System.out.println( "Product_code :\t" + Product_code );
+        System.out.println( "Product_name :\t" + Product_name );
+        System.out.println( "Price :\t" + Price );
+        System.out.println( "Amount :\t" + Amount );
+        System.out.println( "Unit :\t" + Unit );
+    }
+    //tăng/giảm số lượng
+    void changeQty(int q){
+        Amount += q;
+    }
+
+    //thay đổi giá tiền sản phẩm
+    void changePrice(float p){
+        Price = p;
+    }
+
+    //kiểm tra hàng còn/hết hàng.
+    boolean checkStock(){
+        if (Amount > 0) return true;
+        return false;
+    }
+}
