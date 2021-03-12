7eaadc5508d5f6371b0db5d3a6530d85e7923ccd
NguyenDinhHuong30088 <70881463+NguyenDinhHuong30088@users.noreply.github.com>
2021-03-12 12:08:08 +0700

    S

--- src/BaiThi/Main.java
+++ src/BaiThi/Main.java
@@ -1,21 +1,32 @@
 package BaiThi;
 
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
-        Hotel a= new Hotel();
-        a.addHotel("Chuong My","Ha Noi","Nguyen Dinh Huong");
-        a.addHotel("Quoc Oai","Ha Noi","Nguyen Dinh Nam");
-        a.addHotel("My Duc","Ha Noi","Nguyen Phi Cong");
-        a.addHotel("Thuong Tin","Ha Noi","Vu Tri Linh");
-        a.addHotel("Cao Phong","Ha Noi","Nguyen Van Hieu");
-        a.addHotel("Thach That","Ha Noi","Tong Quang Son");
-        a.addHotel("Bien Giang","Ha Noi","Tong Dinh Minh");
-        a.addHotel("Thanh Oai","Ha Noi","Do Duy Thuong");
-        a.addHotel("Ha Dong","Ha Noi","Nguyen Van Manh");
-        a.addHotel("Chuc Son","Ha Noi","Tong Nguyen Giang");
-
-        for (Hotel y:a.HotelList){
-            System.out.println(a.name+a.location+a.ownerName);
+        ArrayList<Hotel> ht = new ArrayList<Hotel>();
+        ht.add(new Hotel("Chuong My", "Ha Noi", "Tong Nguyen Giang"));
+        ht.add(new Hotel("Phung Chau", "Ha Noi", "Nguyen Phi Cong"));
+        ht.add(new Hotel("Tien Phuong", "Ha Noi", "Nguyen Dinh Huong"));
+        ht.add(new Hotel("Tan Hoa", "Ha Noi", "Nguyen Dinh Nam"));
+        ht.add(new Hotel("Tan Phu", "Ha Noi", "Vu Tri Linh"));
+        ht.add(new Hotel("Quoc Oai", "Ha Noi", "Vu Tri Khiem"));
+        ht.add(new Hotel("Dai Thanh", "Ha Noi", "Do Duy Thuong"));
+        ht.add(new Hotel("Ngoc Hoa", "Ha Noi", "Tong Dinh Minh"));
+        ht.add(new Hotel("Dai Yen", "Ha Noi", "Nguyen Van Manh"));
+        ht.add(new Hotel("Thanh Oai", "Ha Noi", "Vu Ngoc Quan"));
+        Scanner scanner = new Scanner(System.in);
+        String ownerName;
+        System.out.println("Nhap Ten Chu Khach San:");
+        ownerName = scanner.nextLine();
+        scanner.close();
+        for (int i = 0; i < ht.size(); i++) {
+            if (ownerName.equals(ht.get(i).getOwnerName())) {
+                System.out.println("Ten Khach San: " + ht.get(i).getName() + "\n Dia Chi: "
+                        + ht.get(i).getLocation() + "\n Ten Chu Khach San: " + ht.get(i).getOwnerName());
+            }
         }
     }
 }
